53d2e81a0f2f88534159c0f07ba64731e2db626a
fengzihcen <1486008923@qq.com>
2023-01-30 16:42:42 +0800

    add ImGui events

--- Land/src/Land/ImGui/ImGuiLayer.h
+++ Land/src/Land/ImGui/ImGuiLayer.h
@@ -2,6 +2,10 @@
 
 #include "Land/Layer.h"
 
+#include "Land/Events/KeyEvent.h"
+#include "Land/Events/MouseEvent.h"
+#include "Land/Events/ApplicationEvent.h"
+
 namespace Land
 {
 	class LAND_API ImGuiLayer : public Layer
@@ -14,6 +18,15 @@ namespace Land
 		void OnDetach();
 		void OnUpdate();
 		void OnEvent(Event& event);
+	private:
+		bool OnMouseButtonPressedEvent(MouseButtonPressedEvent& e);
+		bool OnMouseButtonReleasedEvent(MouseButtonReleasedEvent& e);
+		bool OnMouseMovedEvent(MouseMovedEvent& e);
+		bool OnMouseScrolledEvent(MouseScrolledEvent& e);
+		bool OnKeyPressedEvent(KeyPressedEvent& e);
+		bool OnKeyReleasedEvent(KeyReleasedEvent& e);
+		bool OnKeyTypedEvent(KeyTypedEvent& e);
+		bool OnWindowResizedEvent(WindowResizeEvent& e);
 	private:
 		float m_Time = 0.0f;
 	};
